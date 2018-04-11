a58b9bd39856db3ab0930112be153a73ab73b360
Diego <di3g0d0ming05@gmail.com>
2018-04-11 12:34:30 -0300

    ajuste template admin

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from .models import TypeProduct, Product
 
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'description', 'price', 'stock', 'get_category']
+    search_fields = ['name', 'description']
+    list_per_page = 20
+    list_filter = ['type_product__category']
+    ordering = ['name']
+    autocomplete_lookup_fields = {
+        'name': ['name'],
+        'description': ['description'],
+    }
+
+    def get_category(self, obj):
+        return obj.type_product.category
+
+    get_category.short_description = 'Categoria'
+    get_category.admin_order_field = 'type_product__category'
+
+
 admin.site.register(TypeProduct)
-admin.site.register(Product)
+admin.site.register(Product, AdminProduct)
